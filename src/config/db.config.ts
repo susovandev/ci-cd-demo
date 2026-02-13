dfafc44733bfde8d436e6f93c712e041f66a684f
Susovan Dev <susovandas.dev@gmail.com>
2026-02-13 17:48:47 +0530

    Refactor database connection logic to dynamically import MongoMemoryServer and improve error handling

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,27 +1,35 @@
-// Core Modules
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 
 import envConfig from './env.config.js';
 import Logger from './logger.config.js';
 
-let mongoServer: MongoMemoryServer;
+let mongoServer: any;
 
 export async function connectDB() {
-	if (envConfig.server.node_env === 'test') {
-		mongoServer = await MongoMemoryServer.create();
-		await mongoose.connect(mongoServer.getUri());
-		Logger.info(
-			`Testing database host: ${mongoServer?.getUri()}; Connected to database: ${mongoServer?.getUri()}`,
-		);
-	} else {
-		const connectionInstance = await mongoose.connect(
-			`${envConfig.database.uri}/${envConfig.database.name}`,
-		);
-		Logger.info(`Database host: ${connectionInstance?.connection.host}`);
-		Logger.info(
-			`Connected to database: ${connectionInstance?.connection.db?.databaseName}`,
-		);
+	try {
+		if (envConfig.server.node_env === 'test') {
+			const { MongoMemoryServer } = await import('mongodb-memory-server');
+
+			mongoServer = await MongoMemoryServer.create();
+			const uri = mongoServer.getUri();
+
+			await mongoose.connect(uri);
+
+			Logger.info(`Testing database host: ${uri}`);
+			Logger.info(`Connected to test database`);
+		} else {
+			const connectionInstance = await mongoose.connect(
+				`${envConfig.database.uri}/${envConfig.database.name}`,
+			);
+
+			Logger.info(`Database host: ${connectionInstance.connection.host}`);
+			Logger.info(
+				`Connected to database: ${connectionInstance.connection.db?.databaseName}`,
+			);
+		}
+	} catch (error) {
+		Logger.error('Database connection failed', error);
+		process.exit(1);
 	}
 }
 
